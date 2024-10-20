8243b938f5b0a8000e5be3049e5aa5357cabf1db
Apoorv Mittal <apoorvmitthu@gmail.com>
2024-10-20 17:52:31 +0530

    pushing with errors 2

--- app/src/main/java/com/apoorv/dscnotes/Utility.kt
+++ app/src/main/java/com/apoorv/dscnotes/Utility.kt
@@ -8,17 +8,15 @@ import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 
-object Utility {
-    fun showToast(context: Context?, message: String?) {
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-    }
+object Utility {fun showToast(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
 
-    val collectionReferenceForNotes: CollectionReference
-        get() {
-            val currentUser = FirebaseAuth.getInstance().currentUser
-            return FirebaseFirestore.getInstance().collection("notes")
-                .document(currentUser!!.uid).collection("my_notes")
-        }
+    fun getCollectionReferenceForNotes(): CollectionReference {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        return FirebaseFirestore.getInstance().collection("notes")
+            .document(currentUser!!.uid).collection("my_notes")
+    }
 
     fun timestampToString(timestamp: Timestamp): String {
         return SimpleDateFormat("MM/dd/yyyy").format(timestamp.toDate())
